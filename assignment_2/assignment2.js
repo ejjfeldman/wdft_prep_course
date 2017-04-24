d4c58e5349cab079032ee50a20cb1b5ca1622d75
Erin Feldman <ejjfeldman@gmail.com>
2017-04-24 11:21:00 -0400

    Final

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,7 +3,9 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
-
+for(var number=1; number<=100; number++){
+    console.log(number);
+}
 
 /*
 Exercise #2
@@ -17,3 +19,13 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+
+var result="#";
+
+for(var number=1; number<=7; number++){
+    console.log(result);
+    result+="#";
+}
+
+
